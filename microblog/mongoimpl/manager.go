4b7bb6bfc26b3d4745a8e5afa9f8cc08dedd42eb
broniyasinnik <broniyasinnik@icloud.com>
2025-09-27 21:53:02 +0300

    Added mongo implementation

---
+++ microblog/mongoimpl/manager.go
@@ -0,0 +1,117 @@
+package mongoimpl
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"micro-blog/microblog"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const collName = "posts"
+
+type MongoManager struct {
+	posts *mongo.Collection
+}
+
+func ensureIndexes(ctx context.Context, collection *mongo.Collection) {
+	indexModels := []mongo.IndexModel{
+		{
+			Keys: bson.D{{Key: "author_id", Value: 1}, {Key: "_id", Value: -1}},
+		},
+	}
+	opts := options.CreateIndexes().SetMaxTime(10 * time.Second)
+
+	_, err := collection.Indexes().CreateMany(ctx, indexModels, opts)
+	if err != nil {
+		panic(fmt.Errorf("failed to ensure indexes %w", err))
+	}
+}
+
+func NewMongoManager(mongoURL string, dbName string) *MongoManager {
+	ctx := context.Background()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURL))
+	if err != nil {
+		panic(err)
+	}
+
+	collection := client.Database(dbName).Collection(collName)
+	ensureIndexes(ctx, collection)
+
+	return &MongoManager{
+		posts: collection,
+	}
+}
+
+func (m *MongoManager) AddPost(ctx context.Context, userId string, text string) (microblog.UserPost, error) {
+	usrPost := microblog.UserPost{AuthorId: userId, Text: text, CreatedAt: time.Now()}
+	res, err := m.posts.InsertOne(ctx, usrPost)
+	if err != nil {
+		return microblog.UserPost{}, fmt.Errorf("something went wrong - %w", microblog.ErrStorage)
+	}
+	id := res.InsertedID.(primitive.ObjectID)
+	err = m.posts.FindOne(ctx, bson.M{"_id": id}).Decode(&usrPost)
+	usrPost.PostId = id.Hex()
+	if errors.Is(err, mongo.ErrNoDocuments) {
+		return microblog.UserPost{}, microblog.ErrNotFound
+	}
+	return usrPost, nil
+}
+
+func (m *MongoManager) GetPost(ctx context.Context, postID string) (microblog.UserPost, error) {
+	var resPost microblog.UserPost
+	objID, _ := primitive.ObjectIDFromHex(postID)
+	err := m.posts.FindOne(
+		ctx, bson.M{"_id": objID},
+	).Decode(&resPost)
+	if err != nil {
+		return resPost, err
+	}
+	resPost.PostId = objID.Hex()
+	return resPost, nil
+}
+
+func (m *MongoManager) GetPostsInPage(ctx context.Context, userId string, token string, size uint8) ([]microblog.UserPost, string, error) {
+
+	var posts []microblog.UserPost
+	var filter bson.M
+	if token != "" {
+		objID, _ := primitive.ObjectIDFromHex(token)
+		filter = bson.M{"author_id": userId, "_id": bson.M{"$lt": objID}}
+	} else {
+		filter = bson.M{"author_id": userId}
+	}
+	cursor, err := m.posts.Find(
+		ctx,
+		filter,
+		options.Find().SetSort(bson.D{{Key: "_id", Value: -1}}),
+		options.Find().SetLimit(int64(size)),
+	)
+	if err != nil {
+		return posts, "", err
+	}
+	defer cursor.Close(ctx)
+	for cursor.Next(ctx) {
+		var resPost microblog.UserPost
+		err = cursor.Decode(&resPost)
+		if err != nil {
+			return posts, "", err
+		}
+
+		var doc bson.M
+		err = cursor.Decode(&doc)
+		if err != nil {
+			return posts, "", err
+		}
+		id := doc["_id"].(primitive.ObjectID)
+		resPost.PostId = id.Hex()
+		posts = append(posts, resPost)
+	}
+
+	return posts, posts[len(posts)-1].PostId, nil
+}
---
+++ microblog/mongoimpl/manager_test.go
@@ -0,0 +1,96 @@
+package mongoimpl
+
+import (
+	"context"
+	"fmt"
+	"micro-blog/microblog"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var ctx = context.Background()
+
+func TestManager(t *testing.T) {
+	suite.Run(t, &ManagerSuite{mongoAddr: "mongodb://localhost:27017", mongodbName: "test"})
+}
+
+type ManagerSuite struct {
+	suite.Suite
+
+	mongoAddr   string
+	mongodbName string
+	mongoClient *mongo.Client
+	mongoDB     *mongo.Database
+	manager     microblog.Manager
+}
+
+func (s *ManagerSuite) SetupSuite() {
+	s.manager = NewMongoManager(s.mongoAddr, s.mongodbName)
+
+	mongoClient, err := mongo.Connect(ctx, options.Client().ApplyURI(s.mongoAddr))
+	s.Require().NoError(err)
+	db := mongoClient.Database(s.mongodbName)
+	s.mongoClient = mongoClient
+	s.mongoDB = db
+}
+
+func (s *ManagerSuite) SetupTest() {
+	s.Require().NoError(s.mongoClient.Database(s.mongodbName).Drop(ctx))
+}
+
+//func (s *ManagerSuite) TearDownTest() {
+//	s.Require().NoError(s.mongoClient.Database(s.mongodbName).Drop(ctx))
+//}
+
+func (s *ManagerSuite) TestAddPost() {
+	usr := "broniy"
+	msg := "This is my first post"
+	p, err := s.manager.AddPost(ctx, usr, msg)
+	s.Require().Equal(usr, p.AuthorId)
+	s.Require().Equal(msg, p.Text)
+	s.Require().NoError(err)
+}
+
+func (s *ManagerSuite) addNPosts(usrName string, n int) {
+	for i := 1; i <= 10; i++ {
+		msg := fmt.Sprintf("This is post number %d", i)
+		_, err := s.manager.AddPost(ctx, usrName, msg)
+		s.Require().NoError(err)
+	}
+}
+
+func (s *ManagerSuite) TestGetPost() {
+	usr := "broniy"
+	s.addNPosts(usr, 10)
+
+	post, err := s.manager.GetPost(ctx, usr)
+	s.Require().NoError(err)
+	s.Require().Equal(usr, post.AuthorId)
+	s.Require().Equal("This is post number 10", post.Text)
+}
+
+func (s *ManagerSuite) TestGetPostsInPage() {
+	usr := "bobi"
+	s.addNPosts(usr, 10)
+
+	token := ""
+	var posts []microblog.UserPost
+	var err error
+
+	posts, token, err = s.manager.GetPostsInPage(ctx, usr, token, 5)
+	s.Require().NoError(err)
+	s.Require().Len(posts, 5)
+	s.Require().Equal(usr, posts[0].AuthorId)
+	s.Require().Equal("This is post number 10", posts[0].Text)
+	s.Require().Equal("This is post number 6", posts[4].Text)
+
+	posts, token, err = s.manager.GetPostsInPage(ctx, usr, token, 5)
+	s.Require().Len(posts, 5)
+	s.Require().Equal(usr, posts[0].AuthorId)
+	s.Require().Equal("This is post number 5", posts[0].Text)
+	s.Require().Equal(usr, posts[4].AuthorId)
+	s.Require().Equal("This is post number 1", posts[4].Text)
+}
